4121b2f6db27da479cc419c7c688c011d5bf2b39
queensarena13 <2.84798136e+08+queensarena13@users.noreply.github.com>
2026-08-18 00:37:43 +0000

    Expand homepage discovery sections

--- components/home-content.tsx
+++ components/home-content.tsx
@@ -1,11 +1,15 @@
 import { AdSlot } from "@/components/ad-slot"
 import { Hero } from "@/components/hero"
+import { HomeTopics } from "@/components/home-topics"
+import { HomeTrust } from "@/components/home-trust"
 import { adSlots } from "@/lib/ads"
 
 export function HomeContent() {
   return (
     <div>
       <Hero />
+      <HomeTopics />
+      <HomeTrust />
       <AdSlot slot={adSlots.home} compact />
     </div>
   )
